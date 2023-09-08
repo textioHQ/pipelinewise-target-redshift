218c2a00f867670c94ec0a0db86a6b239aa7fcac
Niall Keleher <niall@textio.com>
2023-09-08 14:18:11 -0700

    Update setup.py
    bump to 1.7.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md") as f:
 
 setup(
     name="pipelinewise-target-redshift",
-    version="1.6.0",
+    version="1.7.0",
     description="Singer.io target for loading data to Amazon Redshift - PipelineWise compatible",
     long_description=long_description,
     long_description_content_type="text/markdown",
